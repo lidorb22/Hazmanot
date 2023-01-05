3e98ea1724ace76aecbed9774265a5b5f689914d
לידור בניסתי <321lidor11@gmail.com>
2023-01-05 16:56:36 +0200

    new api host

--- api/inviteApi.js
+++ api/inviteApi.js
@@ -1,75 +1,74 @@
 import axios from "axios";
 
+const inviteUrl = "https://hazmanot-api.onrender.com/invite/create";
+const allINVUrl = "https://hazmanot-api.onrender.com/invite/allItems";
+const iNVUrl = "https://hazmanot-api.onrender.com/invite/item";
+const delInvUrl = "https://hazmanot-api.onrender.com/invite/delItem";
+const commingUrl = "https://hazmanot-api.onrender.com/invite/comming";
+const readUrl = "https://hazmanot-api.onrender.com/invite/read";
 
-const inviteUrl = 'https://hazmanot-il.herokuapp.com/invite/create';
-const allINVUrl = 'https://hazmanot-il.herokuapp.com/invite/allItems';
-const iNVUrl = 'https://hazmanot-il.herokuapp.com/invite/item';
-const delInvUrl = 'https://hazmanot-il.herokuapp.com/invite/delItem';
-const commingUrl = 'https://hazmanot-il.herokuapp.com/invite/comming';
-const readUrl = 'https://hazmanot-il.herokuapp.com/invite/read';
-
-export const newInvite = FrmData =>{
-    return new Promise(async(resolve, reject) =>{
-        try {
-            const res = await axios.post(inviteUrl, FrmData)
-            resolve(res.data);
-        } catch (error) {
-            reject(error);
-        }
-    })
+export const newInvite = (FrmData) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const res = await axios.post(inviteUrl, FrmData);
+      resolve(res.data);
+    } catch (error) {
+      reject(error);
+    }
+  });
 };
 
-export const commingCount = FrmData =>{
-    return new Promise(async(resolve, reject) =>{
-        try {
-            const res = await axios.post(commingUrl, FrmData)
-            resolve(res.data);
-        } catch (error) {
-            reject(error);
-        }
-    })
+export const commingCount = (FrmData) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const res = await axios.post(commingUrl, FrmData);
+      resolve(res.data);
+    } catch (error) {
+      reject(error);
+    }
+  });
 };
 
-export const allInvList = FrmData =>{
-    return new Promise(async(resolve, reject) =>{
-        try {
-            const res = await axios.post(allINVUrl, FrmData)
-            resolve(res.data);
-        } catch (error) {
-            reject(error);
-        }
-    })
+export const allInvList = (FrmData) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const res = await axios.post(allINVUrl, FrmData);
+      resolve(res.data);
+    } catch (error) {
+      reject(error);
+    }
+  });
 };
 
-export const InvObj = FrmData =>{
-    return new Promise(async(resolve, reject) =>{
-        try {
-            const res = await axios.post(iNVUrl, FrmData)
-            resolve(res.data);
-        } catch (error) {
-            reject(error);
-        }
-    })
+export const InvObj = (FrmData) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const res = await axios.post(iNVUrl, FrmData);
+      resolve(res.data);
+    } catch (error) {
+      reject(error);
+    }
+  });
 };
 
-export const InvDelete = FrmData =>{
-    return new Promise(async(resolve, reject) =>{
-        try {
-            const res = await axios.post(delInvUrl, FrmData)
-            resolve(res.data);
-        } catch (error) {
-            reject(error);
-        }
-    })
+export const InvDelete = (FrmData) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const res = await axios.post(delInvUrl, FrmData);
+      resolve(res.data);
+    } catch (error) {
+      reject(error);
+    }
+  });
 };
 
-export const numberOfEvents = () =>{
-    return new Promise(async(resolve, reject) =>{
-        try {
-            const res = await axios.get(readUrl);
-            resolve(res.data);
-        } catch (error) {
-            reject(error);
-        }
-    })
+export const numberOfEvents = () => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const res = await axios.get(readUrl);
+      resolve(res.data);
+    } catch (error) {
+      reject(error);
+    }
+  });
 };
--- api/userApi.js
+++ api/userApi.js
@@ -1,89 +1,89 @@
 import axios from "axios";
 
-const loginUrl = 'https://hazmanot-il.herokuapp.com/auth/login';
-const registerUrl = 'https://hazmanot-il.herokuapp.com/auth/register';
-const userUrl = 'https://hazmanot-il.herokuapp.com/user/';
-const verifyUrl = 'https://hazmanot-il.herokuapp.com/auth/authorized';
-const reMassageUrl = 'https://hazmanot-il.herokuapp.com/auth/newToken';
-const problemUrl = 'https://hazmanot-il.herokuapp.com/user/problem';
+const loginUrl = "https://hazmanot-api.onrender.com/auth/login";
+const registerUrl = "https://hazmanot-api.onrender.com/auth/register";
+const userUrl = "https://hazmanot-api.onrender.com/user/";
+const verifyUrl = "https://hazmanot-api.onrender.com/auth/authorized";
+const reMassageUrl = "https://hazmanot-api.onrender.com/auth/newToken";
+const problemUrl = "https://hazmanot-api.onrender.com/user/problem";
 
-export const userLogin = FrmData =>{
-    return new Promise(async(resolve, reject) =>{
-        try {
-            const res = await axios.post(loginUrl, FrmData)
-            resolve(res.data);
-        } catch (error) {
-            reject(error);
-        }
-    })
+export const userLogin = (FrmData) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const res = await axios.post(loginUrl, FrmData);
+      resolve(res.data);
+    } catch (error) {
+      reject(error);
+    }
+  });
 };
 
-export const userRegister = FrmData =>{
-    return new Promise(async(resolve, reject) =>{
-        try {
-            const res = await axios.post(registerUrl, FrmData)
-            resolve(res.data);
-        } catch (error) {
-            reject(error);
-        }
-    })
+export const userRegister = (FrmData) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const res = await axios.post(registerUrl, FrmData);
+      resolve(res.data);
+    } catch (error) {
+      reject(error);
+    }
+  });
 };
 
-export const fetchUser = () =>{
-    return new Promise(async(resolve, reject) =>{
-        try {
-            const accessToken = localStorage.getItem('token')
-            if(!accessToken){
-                reject("Token not found!")
-            }
-            const root = localStorage.getItem('userID')
+export const fetchUser = () => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const accessToken = localStorage.getItem("token");
+      if (!accessToken) {
+        reject("Token not found!");
+      }
+      const root = localStorage.getItem("userID");
 
-            const res = await axios.get(userUrl + root, { 
-                headers: {
-                    token: accessToken
-                }
-            })
-            resolve(res.data);
-        } catch (error) {
-            console.log(error.message);
-            reject(error);
-        }
-    })
+      const res = await axios.get(userUrl + root, {
+        headers: {
+          token: accessToken,
+        },
+      });
+      resolve(res.data);
+    } catch (error) {
+      console.log(error.message);
+      reject(error);
+    }
+  });
 };
 
-export const userVerify = FrmData =>{
-    return new Promise(async(resolve, reject) =>{
-        try {
-            const res = await axios.post(verifyUrl, FrmData)
-            if(res.status === 200){
-                resolve(res.data);
-            }
-        } catch (error) {
-            reject(error);
-        }
-    })
+export const userVerify = (FrmData) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const res = await axios.post(verifyUrl, FrmData);
+      if (res.status === 200) {
+        resolve(res.data);
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
 };
 
-export const sendMassageAgain = FrmData =>{
-    return new Promise(async(resolve, reject) =>{
-        try {
-            const res = await axios.post(reMassageUrl, FrmData)
-            if(res.status === 200){
-                resolve(res.data);
-            }
-        } catch (error) {
-            reject(error);
-        }
-    })
+export const sendMassageAgain = (FrmData) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const res = await axios.post(reMassageUrl, FrmData);
+      if (res.status === 200) {
+        resolve(res.data);
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
 };
 
-export const userProblem = FrmData =>{
-    return new Promise(async(resolve, reject) =>{
-        try {
-            const res = await axios.post(problemUrl, FrmData)
-            resolve(res.data);
-        } catch (error) {
-            reject(error);
-        }
-    })
+export const userProblem = (FrmData) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const res = await axios.post(problemUrl, FrmData);
+      resolve(res.data);
+    } catch (error) {
+      reject(error);
+    }
+  });
 };
